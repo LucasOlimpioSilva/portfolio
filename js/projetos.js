d6a7273921e1d844b8f572b71bfe1800eb6bf8fd
Lucas <lucasolimpio267@gmail.com>
2026-09-03 23:27:57 -0300

    Feat: adicionando breakpoints de resposibilidade e ajustes de layout

--- js/projetos.js
+++ js/projetos.js
@@ -9,6 +9,17 @@ const projetos =  [
         tecnologias: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
         linkProjeto: 'https://lucasolimpiosilva.github.io/Site-AndersonGuiaTurista/',
         linkRepositorio: 'https://github.com/lucasolimpiosilva/Site-AndersonGuiaTurista'
+    },
+
+    {
+        nome: 'Anderson Turismo',
+        imgDesktop: './assets/projects/anderson-turismo/desktop.png',
+        imgMobile: './assets/projects/anderson-turismo/mobile.png',
+        descricao: 'Projeto de site para uma empresa de turismo, com informações sobre os serviços oferecidos e um formulário de contato.',
+        descricaoCompleta: 'Projeto de site para uma empresa de turismo, com informações sobre os serviços oferecidos e um formulário de contato. O site foi desenvolvido utilizando HTML, CSS, JavaScript e Bootstrap, e é responsivo para diferentes tamanhos de tela.',
+        tecnologias: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+        linkProjeto: 'https://lucasolimpiosilva.github.io/Site-AndersonGuiaTurista/',
+        linkRepositorio: 'https://github.com/lucasolimpiosilva/Site-AndersonGuiaTurista'
     }
 ]
 
